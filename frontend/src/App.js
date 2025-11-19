7997512c715ea5dc63b2850c76d00e99ed2d9332
phiona-dev <phionadev@gmail.com>
2025-11-19 21:43:55 +0300

    comment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,13 +12,13 @@ import LoginPage from './components/login/login';
 import CreateProfilePage from './components/create-profile/createProfile';
 import ProfilePage from './components/create-profile/profilePage';
 import MatchingPage from './components/match/match';
-import StartupRedirect from './components/auth/StartupRedirect';
+//import StartupRedirect from './components/auth/StartupRedirect';
   
 function App() {
   return (
     <BrowserRouter>
       <div style={{ minHeight: '100vh' }}>
-        <StartupRedirect/>
+        {/*<StartupRedirect/>*/}
         <Routes>
           {/*<Route path="/" element={<HomePage />} />*/}
           <Route path="/chats" element={<ChatPage />} />
